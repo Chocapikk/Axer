6e6e6d7946cc692a3160621844fd2ae43dff04e6
λ <138330732+0romos@users.noreply.github.com>
2023-11-16 11:19:47 +0100

    commit: create wifi.py

---
+++ x64/wifi.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+
+from data.logger import Logger
+
+class Log:
+
+    def msg(status):
+        Logger.__client_logger__(status, "logs.vs", "./logs/")
+
+
+class Connection:
+
+    @staticmethod
+    def check_wifi():
+        try:
+            result = subprocess.run(['iwconfig'], capture_output=True, text=True)
+            output = result.stdout
+            if 'ESSID:off/' in output:
+                Log.msg("Make sure you have an active wifi connection")
+                sys.exit()
+            else:
+                Log.msg("Active wifi connection found")
+        except FileNotFoundError:
+            Log.msg("Error: 'iwconfig' command not found. Run this on a Linux system with wireless support.")
+            sys.exit()
