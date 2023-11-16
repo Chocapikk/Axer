1639ddfe93fea6635dacc4ed540fcd5302149665
λ <138330732+0romos@users.noreply.github.com>
2023-11-16 10:52:48 +0100

    commit: update help.py

--- modules/help.py
+++ modules/help.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class HelpCommand:
     """
     A class to display a help menu for available exploit modules.
@@ -6,25 +9,46 @@ class HelpCommand:
     about available exploit modules, including their names, authors, creation dates, and descriptions.
 
     Methods:
-        execute(exploits): Display the help menu for available exploit modules.
-
-    Attributes:
-        None
+        __init__: Initialize a HelpCommand object.
+        load_help_commands: Load the help_commands_json from the specified file.
+        execute: Display a formatted help menu for available exploit modules.
+        execute_command: Execute a specific command with optional arguments.
     """
 
     def __init__(self):
-        """Initialize a HelpCommand object (no specific attributes required)."""
-        pass
+        """Initialize a HelpCommand object."""
+        self.commands = {
+            "help": None,
+            "exit": None,
+            "clear": lambda: os.system("clear"),
+            "exploits": Subcommands.list_exploits,
+            "search": None
+        }
 
-    def execute(self, exploits):
+        self.help_commands_json = self.load_help_commands()
+
+    def load_help_commands(self):
+        """
+        Load the help_commands_json from the specified file.
+
+        Returns:
+            dict: The loaded JSON structure.
+        """
+        file_path = os.path.join(os.path.dirname(__file__), "help_commands", "struct.json")
+        with open(file_path, "r") as file:
+            return json.load(file)
+
+    def execute(self, exploits, folder_name=None):
         """
         Display a formatted help menu for available exploit modules.
 
         This method prints a formatted help menu that lists the available exploit modules,
-        including their names, authors, creation dates, and descriptions.
+        including their names, authors, creation dates, and descriptions. It also supports
+        filtering the display based on a specified folder.
 
         Args:
             exploits (list): A list of exploit instances to be displayed in the help menu.
+            folder_name (str, optional): The name of the folder to filter exploits. Default is None.
         """
         print("""
     Help Menu
@@ -32,6 +56,58 @@ class HelpCommand:
         
     #   Name               Author         Date              Description
     -   ----               ------         ----              -----------""")
-        for i, exploit_instance in enumerate(exploits, 1):
-            print(f"    {i: <4}{exploit_instance.name: <19}{exploit_instance.author: <15}{exploit_instance.creation_date: <18}{exploit_instance.description}")
-        print()
+        
+        if folder_name:
+            filtered_exploits = [exploit for exploit in exploits if exploit.folder == folder_name]
+            if filtered_exploits:
+                for i, exploit_instance in enumerate(filtered_exploits, 1):
+                    print(f"    {i: <4}{exploit_instance.name: <19}{exploit_instance.author: <15}{exploit_instance.creation_date: <18}{exploit_instance.description}")
+                print()
+            else:
+                print(f"No exploits found in the '{folder_name}' folder. Use 'help' to see available commands.")
+        else:
+            for i, command in enumerate(self.help_commands_json["commands"], 1):
+                print(f"    {i: <4}{command['name']: <19}{command['author']: <15}{command['date']: <18}{command['description']}")
+            print("")
+
+    def execute_command(self, command, *args):
+        """
+        Execute a specific command with optional arguments.
+
+        Args:
+            command (str): The command to execute.
+            *args: Optional arguments for the command.
+        """
+        if command in self.commands:
+            self.commands[command](*args)
+        else:
+            print(f"Command '{command}' not found in available commands.")
+
+
+class Subcommands:
+    """
+    A class containing subcommands for the HelpCommand class.
+
+    Methods:
+        list_exploits: Display a list of available exploit modules.
+    """
+
+    @staticmethod
+    def list_exploits():
+        """
+        Display a list of available exploit modules.
+        """
+        exploits_path = "exploits/"
+        folders = [folder for folder in os.listdir(exploits_path) if os.path.isdir(os.path.join(exploits_path, folder))]
+        
+        print("""
+    Exploits
+    =========
+
+    #   Name
+    -   ----""")
+
+        for i, folder in enumerate(folders, 1):
+            print(f"    {i: <4}{folder}")
+        
+        print("\nSyntax: help <exploit-name>\n")
